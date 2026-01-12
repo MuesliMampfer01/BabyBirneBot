463fbae422d9ad61f5d65382ab81c0cc559fcc22
Muesli <roman.lentfoehr@stud.uni-due.de>
2026-01-12 12:19:54 +0100

    punktesystem hinzugefügt

--- cogs/gambling.py
+++ cogs/gambling.py
@@ -9,17 +9,52 @@ class Gambling(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="coinflip",aliases=["cf", "münze"], help="mache einen Coinflip")
-    async def coinflip(self,ctx):
-        seiten =["Kopf", "Zahl"]
-        ergebnis = random.choice(seiten)
-        await ctx.send(f"{ctx.author.mention} hat eine Münze geworfen... \nDas Ergebnis ist {ergebnis}")
+    async def give_reward(self, ctx, amount):
+        punktesys = self.bot.get_cog('Punktesystem')
 
-    @commands.command(name="zahlenraten",aliases=["zr", "raten"],help="errate die gesuchte Zahl")
-    async def zahlenraten(self,ctx):
+        if punktesys:
+            punktesys.add_points(ctx.author.id, ctx.guild.id, amount)
+            return True
+        else:
+            print("Punktesystem konnte nicht geladen werden, keine Punkte vergeben")
+            return False
+
+
+    @commands.command(name="coinflip",aliases=["cf", "münze"], help="mache einen Coinflip")
+    async def coinflip(self,ctx, wahl: str):
+        if not wahl:
+            await ctx.send("Bitte wähle 'Kopf' oder 'Zahl'! Bsp: '!cf kopf'")
+            return
+
+        seiten = ["kopf", "zahl"]
+        wahl = wahl.lower()
+        if wahl not in seiten:
+            await ctx.send("Es gibt nur 'kopf' oder 'Zahl'")
+            return
+
+        ergebnis = random.choice(seiten)
+
+        msg = await ctx.send("Die Münze wurde geworfen...")
+        await asyncio.sleep(1)
+
+        if wahl == ergebnis:
+            punkte = 10
+            hat_punkte_bekommen = await self.give_reward(ctx, punkte)
+
+            text = f"**{ergebnis.capitalize()}!** Du hast gewonnen!"
+            if hat_punkte_bekommen:
+                text += f"\n **+{punkte} Punkte** wurden dir gutgeschrieben"
+
+            await msg.edit(content=text)
+        else:
+            await msg.edit(content=f"**{ergebnis.capitalize()}!** Schade, leider ist es die Falsche Zahl")
+
+
+    @commands.command(name="zahlenraten",aliases=["zr", "raten"],help="Errate die gesuchte Zahl")
+    async def zahlenraten(self, ctx, zahl: int = None):
         gesuchte_zahl = random.randint(1, 100)
         versuche = 5
-        await ctx.send("Willkommen beim Zahlenraten!\nEs wird eine Zahl zwischen 1 und 100 gesucht.\nDu hast 5 Versuche um die geheime Zahl zu finden!")
+        await ctx.send("**Willkommen beim Zahlenraten!**\nEs wird eine Zahl zwischen 1 und 100 gesucht.\nDu hast 5 Versuche um die geheime Zahl zu finden!")
 
         def check(m):
             return m.author == ctx.author and m.channel == ctx.channel
@@ -29,8 +64,14 @@ class Gambling(commands.Cog):
                 msg = await self.bot.wait_for("message", check=check, timeout=60.0)
                 geratene_zahl = int(msg.content)
 
-                if geratene_zahl == gesuchte_zahl:
-                    await ctx.send(f"Du hast die richtige Zahl gefunden!  Die gesuchte Zahl war {gesuchte_zahl}!")
+                if geratene_zahl == gesuchte_zahl:#
+                    punkte = 50
+                    hat_punkte_bekommen = await self.give_reward(ctx, punkte)
+
+                    zusatz = ""
+                    if hat_punkte_bekommen:
+                        zusatz = f"\n **+{punkte} Punkte** wurden dir gutgeschrieben "
+                    await ctx.send(f"Du hast die richtige Zahl gefunden!  Die gesuchte Zahl war {gesuchte_zahl}!{zusatz}")
                     break
 
                 versuche -= 1
@@ -51,6 +92,7 @@ class Gambling(commands.Cog):
                 await ctx.send(f"Zu langsam! Die gesuchte Zahl war {gesuchte_zahl}")
                 break
 
+
     @commands.command(name="hirse",help="hirse")
     async def hirse(self,ctx):
         try:
@@ -59,6 +101,7 @@ class Gambling(commands.Cog):
                 await ctx.send(file=pic)
         except FileNotFoundError:
             await ctx.send("Bild nicht gefunden")
+
 
     @commands.command(name="frosch",aliases=["frog", "quak"],help="Zeigt einen zufälligen Frosch")
     async def frosch(self,ctx):
---
+++ cogs/pointsystem.py
@@ -0,0 +1,96 @@
+import discord
+from discord.ext import commands
+import sqlite3
+import os
+
+class Pointsystem(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.db_path = "./data/points.db"
+        self.setup_db()
+
+        def setup_db(self):
+            os.makedirs("./data", exist_ok=True)
+            #Telefonverbindung in Lagerhalle
+            with sqlite3.connect(self.db_path) as conn:
+                cursor = conn.cursor()
+
+            cursor.execute("""
+                CREATE TABLE IF NOT EXISTS serv_points (
+                    user_id INTEGER,
+                    serv_id INTEGER,
+                    points INTEGER DEFAULT 0,
+                    PRIMARY KEY (user_id, serv_id))
+                    """)
+            conn.commit()
+
+        #Hilfsmethoden
+        def add_points(self, user_id, serv_id, amount):
+            #automatisch öffnen und schließen der Verbindung am Ende des Blocks
+            with sqlite3.connect(self.db_path) as conn:
+                cursor = conn.cursor()
+
+                #neuer Eintrag falls nicht vorhanden
+                cursor.execute("""
+                    INSERT OR IGNORE INTO serv_points (user_id, serv_id, points)
+                    VALUES (?, ?, 0)
+                    """, (user_id, serv_id))
+
+                #hinzufügen neuer Punkte
+                cursor.execute("""
+                    UPDATE serv_points
+                    SET points = points + ?
+                    WHERE user_id = ? AND serv_id = ?
+                    """, (amount, user_id, serv_id))
+
+                conn.commit()
+
+        def get_points(self, user_id, serv_id):
+            with sqlite3.connect(self.db_path) as conn:
+                cursor = conn.cursor()
+                cursor.execute("""
+                    SELECT points FROM serv_points 
+                    WHERE user_id = ? AND serv_id = ?
+                    """, (user_id, serv_id))
+
+                res = cursor.fetchone()
+                return res[0] if res else 0
+
+
+        @commands.command(name="points", aliases=["punkte", "p"], help="Zeigt deinen Punktestand an")
+        async def balance(self, ctx):
+            points = self.get_points(ctx.author.id, ctx.guild.id)
+            await ctx.reply(f"Du hast aktuell auf {ctx.guild.name} **{points}** Punkte")
+
+        @commands.command(name="top", aliases=["leaderboard"], help="Zeigt die Bestenliste dieses Servers an")
+        async def leaderboard(self, ctx):
+            with sqlite3.connect(self.db_path) as conn:
+                cursor = conn.cursor()
+                cursor.execute("""
+                    SELECT user_id, points FROM serv_points
+                    WHERE serv_id = ?
+                    ORDER BY points DESC LIMIT 10
+                    """, (ctx.guild.id,))
+
+                top_users = cursor.fetchall()
+
+            if not top_users:
+                await ctx.send("Auf diesem Server hat noch niemand Punkte!")
+                return
+
+            embed  = discord.Embed(title=f"🏆 Top 10 auf {ctx.guild.name} 🏆", color=discord.Color.gold())
+            text = ""
+            for i, (user_id, points) in enumerate(top_users, start=1):
+                member = ctx.guild.get_member(user_id)
+                name = member.display_name if member else "Unbekannter User"
+
+                medaille = "🥇" if i == 1 else "🥈" if i == 2 else "🥉" if i == 3 else f"#{i}"
+                text += f"{medaille} {name}: {points} Punkte\n"
+
+            embed.description = text
+            await ctx.send(embed=embed)
+
+async def setup(bot):
+    await bot.add_cog(Pointsystem(bot))
+
+
