2858ccfb641e6e122263832eb9d197bd48c4dcfb
Roman Lentföhr <roman.lentfoehr@stud.uni-due.de>
2026-08-04 01:21:57 +0200

    !weather in /weather umgewandelt

--- cogs/weather.py
+++ cogs/weather.py
@@ -1,6 +1,7 @@
 import aiohttp
 import discord
 from discord.ext import commands
+from discord import app_commands
 
 class Weather(commands.Cog):
     def __init__(self, bot):
@@ -24,10 +25,12 @@ class Weather(commands.Cog):
             return "⛈️", "Gewitter"
         return "🌡️", "Unbekannt"
 
-    @commands.cooldown(1, 5, commands.BucketType.user)
-    @commands.command(name="weather", aliases=["w", "wetter"], help="Zeigt das aktuelle Wetter an: !weather <Ort>")
+
+    @app_commands.command(name="weather", description="Zeigt das aktuelle Wetter an eines Wunschortes an")
+    @app_commands.describe(city="Gebe einen Ort ein")
+    @app_commands.checks.cooldown(1, 5, key=lambda i: i.guild_id or i.user_id)
     # * = Name geht auch mit Leerzeichen
-    async def weather(self, ctx, *, city: str):
+    async def weather(self, interaction: discord.Interaction, *, city: str):
 
         #Daten von FrogAPI abrufen
         try:
@@ -35,16 +38,16 @@ class Weather(commands.Cog):
                 #Stadt wird als Parameter an die FrogAPI übergeben
                 async with session.get(self.api_url, params={"city": city}) as resp:
                     if resp.status == 404:
-                        await ctx.send(f"Konnte den Ort **{city}** auf der Landkarte nicht finden...")
+                        await interaction.response.send_message(f"Konnte den Ort **{city}** auf der Landkarte nicht finden...")
                         return
                     elif resp.status != 200:
-                        await ctx.send("FrogAPI gerade nicht erreichbar")
+                        await interaction.response.send_message("FrogAPI gerade nicht erreichbar")
                         return
 
                     data = await resp.json()
 
         except Exception as e:
-            await ctx.send(f"Verbindungsfehler: {e}")
+            await interaction.response.send_message(f"Verbindungsfehler: {e}")
             return
 
         #Wetter in Emojis übersetzen
@@ -52,7 +55,14 @@ class Weather(commands.Cog):
         temperature = data["temperature"]
 
         #dynamische Farbe je nach Temperatur
-        embed_color = discord.Color.blue() if temperature < 15 else discord.Color.orange()
+        if temperature <= 15:
+            embed_color = discord.Color.blue()
+
+        elif 15 > temperature  > 25:
+            embed_color = discord.Color.orange()
+
+        else:
+            embed_color = discord.Color.red()
 
         #Embed aufbauen
         embed = discord.Embed(
@@ -64,7 +74,7 @@ class Weather(commands.Cog):
         embed.add_field(name="Aktuell", value=f"{temperature} °C", inline=True)
         embed.add_field(name="Gefühlt", value=f"{data['feels_like']} °C", inline=True)
 
-        await ctx.send(embed=embed)
+        await interaction.followup.send(embed=embed)
 
 async def setup(bot):
     await bot.add_cog(Weather(bot))
